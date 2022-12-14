5e33511c70131088d5609987aee114cffb7be981
Fatma Ikış <77547782+fatmaikis@users.noreply.github.com>
2022-12-14 19:46:23 +0300

    Add files via upload

---
+++ SinemaUygulaması/SinemaUygulaması/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace SinemaUygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.biletalbtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(120, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kat : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(120, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Salon : ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0. Kat ",
+            "1.Kat "});
+            this.comboBox1.Location = new System.Drawing.Point(206, 78);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "A ",
+            "B",
+            "C "});
+            this.comboBox2.Location = new System.Drawing.Point(206, 133);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 28);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged_1);
+            // 
+            // biletalbtn
+            // 
+            this.biletalbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.biletalbtn.Location = new System.Drawing.Point(425, 133);
+            this.biletalbtn.Name = "biletalbtn";
+            this.biletalbtn.Size = new System.Drawing.Size(110, 44);
+            this.biletalbtn.TabIndex = 4;
+            this.biletalbtn.Text = "Bilet Al";
+            this.biletalbtn.UseVisualStyleBackColor = true;
+            this.biletalbtn.Click += new System.EventHandler(this.biletalbtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(393, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(50, 20);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.DoubleClick += new System.EventHandler(this.textBox1_DoubleClick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(449, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(50, 20);
+            this.textBox2.TabIndex = 17;
+            this.textBox2.DoubleClick += new System.EventHandler(this.textBox2_DoubleClick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(505, 86);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(50, 20);
+            this.textBox3.TabIndex = 18;
+            this.textBox3.DoubleClick += new System.EventHandler(this.textBox3_DoubleClick);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(561, 86);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(50, 20);
+            this.textBox4.TabIndex = 19;
+            this.textBox4.DoubleClick += new System.EventHandler(this.textBox4_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(550, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 44);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Bilet İptali";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(445, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 24);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "----------";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(772, 316);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.biletalbtn);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button biletalbtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SinemaUygulaması/SinemaUygulaması/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SinemaUygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //kat, salon, koltuk
+        string[,,] sinema = new string[2, 3, 4];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sinema[0, 0, 0] = "X"; //0.kat 0.salon 0.ve 1.koltuk dolu
+            sinema[0, 0, 1] = "X";
+            
+        }
+
+       
+
+        private void biletalbtn_Click(object sender, EventArgs e)
+        {
+            int katNo = comboBox1.SelectedIndex;
+            int salon = comboBox2.SelectedIndex; 
+
+            if (textBox1.Text == "X")
+            {
+                sinema[katNo, salon, 0] = "X";
+            }
+            if (textBox2.Text == "X")
+            {
+                sinema[katNo, salon, 1] = "X";
+            }
+            if (textBox3.Text == "X")
+            {
+                sinema[katNo, salon, 2] = "X";
+            }
+            if (textBox4.Text == "X")
+            {
+                sinema[katNo, salon, 3] = "X";
+            }
+
+
+        }
+
+        private void textBox3_DoubleClick(object sender, EventArgs e)
+        {
+            textBox3.Text = "X";
+           
+        }
+
+        private void textBox4_DoubleClick(object sender, EventArgs e)
+        {
+            textBox4.Text = "X";
+           
+        }
+
+        private void comboBox2_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            textBox1.ReadOnly = false;
+            textBox2.ReadOnly = false;
+            textBox3.ReadOnly = false;
+            textBox4.ReadOnly = false;
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+
+
+            int katBoyut = sinema.GetLength(0);
+            int salonBoyut = sinema.GetLength(1);
+            int secimKat = comboBox1.SelectedIndex;
+            int secimSalon = comboBox2.SelectedIndex;
+
+
+            for (int kat = 0; kat < katBoyut; kat++)
+            {
+                for (int salon = 0; salon < salonBoyut; salon++)
+                {
+                    for (int koltuk = 0; koltuk < 4; koltuk++)
+                    {
+                        if (sinema[secimKat, secimSalon, koltuk] == "X")
+                        {
+                            switch (koltuk)
+                            {
+                                case 0:
+                                    textBox1.Text = "X";
+                                    textBox1.ReadOnly = true;
+                                    break;
+                                case 1:
+                                    textBox2.Text = "X";
+                                    textBox2.ReadOnly = true;
+                                    break;
+                                case 2:
+                                    textBox3.Text = "X";
+                                    textBox3.ReadOnly = true;
+                                    break;
+                                case 3:
+                                    textBox4.Text = "X";
+                                    textBox4.ReadOnly = true;
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "X")
+            {
+                textBox1.Text = "";
+                label3.Text = "1.Koltuk iptal edildi";
+            }
+            else if (textBox2.Text == "X")
+            {
+                textBox2.Text = "";
+                label3.Text = "2.Koltuk iptal edildi";
+            }    
+            else if (textBox3.Text == "X")
+            {
+                textBox3.Text = "";
+                label3.Text = "3.Koltuk iptal edildi";
+            }
+            else if (textBox4.Text=="X")
+            {
+                textBox4.Text = "";
+                label3.Text = "4.Koltuk iptal edildi"; 
+            }
+            
+            
+            
+            textBox4.Text = "";
+        }
+
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+            textBox1.Text = "X";
+            
+            
+
+        }
+
+        private void textBox2_DoubleClick(object sender, EventArgs e)
+        {
+            textBox2.Text = "X";
+
+            
+        }
+    }
+}
